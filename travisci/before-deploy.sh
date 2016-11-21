8c9dcb668cc1c1048557d01a354f15bb18465250
achatain <antoine86roux@gmail.com>
2016-11-21 11:24:42 +0000

    Fixed script

--- travisci/before-deploy.sh
+++ travisci/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_7d915bc7a3f5_key -iv $encrypted_7d915bc7a3f5_iv -in travisci/codesigning.asc.enc -out travisci/codesigning.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import travisci/codesigning.asc
 fi
